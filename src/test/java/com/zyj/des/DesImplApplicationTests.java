ccbfec13d2080fccdbba77ccb03427e87cd9c199
冰咖啡 <z1876849563@163.com>
2026-01-12 09:34:23 +0800

    init

---
+++ src/test/java/com/zyj/des/DesImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyj.des;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
